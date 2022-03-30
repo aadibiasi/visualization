ee882bb5e3c9b0c2724eeaf591cf5ff7ed6e3a2b
ASinanSaglam <asinansaglam@gmail.com>
2022-03-30 12:08:11 -0400

    different colored markers, del(t) of 0.1 and a sample movie added

--- plotter.py
+++ plotter.py
@@ -3,10 +3,12 @@ from state import State
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
+import os
 
 class Plotter:
     def __init__(self, options=None):
         self.options = options
+        self.colors = ["c", "m", "y", "k", "g", "b", "r", "0.5"]
 
     def plot(self, state, ax=None):
         ribos = state.ribos
@@ -15,8 +17,8 @@ class Plotter:
         ax.axhline(y=mrna.y, color='k', linestyle='-', linewidth=3)
         for iribo,ribo in enumerate(ribos):
             if ribo.pos != -1:
-                print(f"Ribo ind: {iribo}, ribo pos: {ribo.pos}")
-                ax.plot(ribo.pos, mrna.y, 'ro')
+                # print(f"Ribo ind: {iribo}, ribo pos: {ribo.pos}")
+                ax.plot(ribo.pos, mrna.y, marker="o", color=self.colors[iribo])
         
 
 if __name__ == '__main__':
@@ -24,9 +26,10 @@ if __name__ == '__main__':
     LH = LogicHandler('model.rxns.tsv')
     # make a plotter
     P = Plotter()
-    for i in [1,10,100]:
-        print("###")
-        print(f"current time: {i}")
+    import numpy as np
+    for i in np.arange(0,101,0.1):
+        # print("###")
+        # print(f"current time: {i}")
         # instantiate an axis
         ax = plt.gca()
         # get the state you want to plot
@@ -36,6 +39,12 @@ if __name__ == '__main__':
         # save the plot
         plt.ylim([0,1])
         plt.xlim([0,101])
-        plt.savefig(f"test_{i:04d}.png")
+        plt.savefig(os.path.join("frames", f"test_{int(i*10):05d}.png"))
         plt.close()
         # break
+    # stitch together the movie with mencoder after
+    # sample command 
+    # ..\..\mplayer\mencoder.exe "mf://*.png" 
+    # -mf fps=60:type=png -ovc lavc -lavcopts 
+    # vcodec=mpeg4:mbd=2:trell:vbitrate=7000 
+    # -vf scale=1024:768 -oac copy -o movie.avi
